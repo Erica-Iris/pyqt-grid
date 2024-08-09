14a3d2456a0b8ae618247cc4647c6d40a91de6af
1ris_W <ediath462@gmail.com>
2024-08-09 21:51:53 +0800

    refactor: paint event
    重构了绘制事件当中的众多语句，抽离成单独函数，并且将一些qpen的配置单独列出

--- grid.py
+++ grid.py
@@ -4,9 +4,12 @@ from PyQt5.QtCore import Qt
 
 
 class Grid(QWidget):
+    cBASELINE = Qt.yellow
+    pBorderLine = QPen(Qt.black, 2, Qt.SolidLine)
+    pBaseLine = QPen(cBASELINE, 0.3, Qt.SolidLine)
+
     def __init__(self):
         super().__init__()
-        # self.current_center_pos_offset = QPoint(0, 0)
         self.h = self.size().width()
         self.w = self.size().height()
         self.current_center_pos_offset_x = 0
@@ -17,14 +20,7 @@ class Grid(QWidget):
 
         self.zoom_level = 1
         self._zoom_factor = 1.05
-        self._zoom_clamp = [0.2, 4]
-
-        self.info_list = []
-        self.info_list.append(self.last_drag_x)
-        self.info_list.append(self.last_drag_y)
-        self.info_list.append(self.current_center_pos_offset_x)
-        self.info_list.append(self.current_center_pos_offset_y)
-        self.info_list.append(self.zoom_level)
+        self._zoom_clamp = [0.3, 4]
 
     def initUI(self):
         self.setAutoFillBackground(True)
@@ -61,9 +57,6 @@ class Grid(QWidget):
         print("mouse release")
 
     def wheelEvent(self, a0):
-        # print("=" * 60)
-        # print("angleDelta: ", a0.angleDelta().y())
-        # print("pixelDelta: ", a0.pixelDelta())
         zoom_factor = 1
         delta_x = self.current_center_pos_offset_x + self.w / 2 - a0.pos().x()
         delta_y = self.current_center_pos_offset_y + self.h / 2 - a0.pos().y()
@@ -87,19 +80,7 @@ class Grid(QWidget):
         # self.repaint()
         self.update()
 
-    def paintEvent(self, a0):
-        qp = QPainter()
-        qp.begin(self)
-
-        size = self.size()
-        self.w = size.width()
-        self.h = size.height()
-
-        qp.setRenderHint(QPainter.Antialiasing)
-        pen = QPen(Qt.white, 0.25, Qt.DotLine)
-        qp.setPen(pen)
-        qp.setBrush(Qt.blue)
-
+    def draw_grid(self, qp):
         # draw grid
         for x in range(
             int(self.w / 2 + self.current_center_pos_offset_x),
@@ -125,27 +106,30 @@ class Grid(QWidget):
             -int(50 * self.zoom_level),
         ):
             qp.drawLine(0, int(y), self.w, int(y))
-        pen = QPen(Qt.yellow, 0.3, Qt.SolidLine)
-        qp.setPen(pen)
-        qp.drawLine(
-            int(self.w / 2 + self.current_center_pos_offset_x),
-            0,
-            int(self.w / 2 + self.current_center_pos_offset_x),
-            self.h,
-        )
-        qp.drawLine(
-            0,
-            int(self.h / 2 + self.current_center_pos_offset_y),
-            self.w,
-            int(self.h / 2 + self.current_center_pos_offset_y),
-        )
-        pen = QPen(Qt.black, 2, Qt.SolidLine)
-        qp.setPen(pen)
-        # draw border
-        qp.drawLine(0, 0, 0, self.h)
-        qp.drawLine(0, self.h, self.w, self.h)
-        qp.drawLine(self.w, self.h, self.w, 0)
-        qp.drawLine(self.w, 0, 0, 0)
+        if (
+            self.current_center_pos_offset_x < -self.w / 2
+            or self.current_center_pos_offset_x > self.w / 2
+        ):
+            pass
+
+    def draw_baseline(self, qp):
+        qp.setPen()
+        if 0 < self.current_center_pos_offset_x + self.w / 2 < self.w:
+            qp.drawLine(
+                int(self.w / 2 + self.current_center_pos_offset_x),
+                0,
+                int(self.w / 2 + self.current_center_pos_offset_x),
+                self.h,
+            )
+        if 0 < self.current_center_pos_offset_y + self.h / 2 < self.h:
+            qp.drawLine(
+                0,
+                int(self.h / 2 + self.current_center_pos_offset_y),
+                self.w,
+                int(self.h / 2 + self.current_center_pos_offset_y),
+            )
+
+    def draw_info(self, qp):
         qp.drawText(20, 20, "last drag x: " + str(self.last_drag_x))
         qp.drawText(20, 30, "last drag y: " + str(self.last_drag_y))
         qp.drawText(
@@ -159,10 +143,42 @@ class Grid(QWidget):
             "current center pos offset y: " + str(self.current_center_pos_offset_y),
         )
         qp.drawText(20, 60, "center pos : " + str(self.last_drag_x))
-        qp.drawPoint(int(self.w / 2), int(self.h / 2))
+
+    def draw_border(self, qp):
+        # draw border
+
+        qp.setPen(self.pBorderLine)
+        qp.drawLine(0, 0, 0, self.h)
+        qp.drawLine(0, self.h, self.w, self.h)
+        qp.drawLine(self.w, self.h, self.w, 0)
+        qp.drawLine(self.w, 0, 0, 0)
+
+    def paintEvent(self, a0):
+        qp = QPainter()
+        qp.begin(self)
+
+        size = self.size()
+        self.w = size.width()
+        self.h = size.height()
+
+        qp.setRenderHint(QPainter.Antialiasing)
+        pen = QPen(Qt.white, 0.25, Qt.DotLine)
+        qp.setPen(pen)
+        qp.setBrush(Qt.blue)
+
+        self.draw_grid(qp)
+        self.draw_baseline(qp)
+        self.draw_border(qp)
+        self.draw_info(qp)
+        self.draw_center_point(qp)
+
         qp.end()
 
     def mouseDoubleClickEvent(self, a0):
         self.current_center_pos_offset_x = 0
         self.current_center_pos_offset_y = 0
         self.repaint()
+
+    def draw_center_point(self, qp):
+        qp.setPen(QPen(Qt.red, 3, Qt.DashLine))
+        qp.drawPoint(int(self.w / 2), int(self.h / 2))
